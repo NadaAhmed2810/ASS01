aeea7156540e31543cb815b82d3fe077221fa414
NadaAhmed2810 <nwrabsywny534@gmail.com>
2024-12-11 19:38:46 +0200

    Question1

---
+++ assignment/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Q1:Write a program that allows the user to enter a number then print it.
+            Console.WriteLine("Enter a Number :");
+            int num;
+            num=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(num);
+            #endregion
+
+        }
+    }
+}
