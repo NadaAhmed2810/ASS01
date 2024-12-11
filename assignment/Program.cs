6de0e4b6723e1ef31bd419aca1d7275d82d65bad
NadaAhmed2810 <nwrabsywny534@gmail.com>
2024-12-11 19:43:28 +0200

    Question2

--- assignment/Program.cs
+++ assignment/Program.cs
@@ -10,6 +10,15 @@
             num=Convert.ToInt32(Console.ReadLine());
             Console.WriteLine(num);
             #endregion
+            #region Q2:Write C# program that Assigning one value type variable to another and modifying the value of one variable and mention what will happen
+            int x = 5;
+            int y = 10;
+            x = y;
+            x++;
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+
+            #endregion
 
         }
     }
