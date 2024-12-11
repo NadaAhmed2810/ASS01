01f9b4c21a1796dc202f5d822118a35fd2a636eb
NadaAhmed2810 <nwrabsywny534@gmail.com>
2024-12-11 16:26:57 +0200

    Error Types

--- ASS01/Program.cs
+++ ASS01/Program.cs
@@ -1,10 +1,33 @@
-﻿namespace ASS01
+﻿using System.ComponentModel;
+
+namespace ASS01
 {
     internal class Program
     {
+        static int  Sum(int x,int y)
+        {
+            return x - y;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Error Types_complication Error
+            #region Syntax error
+            //Console.WriteLine("hello");
+            //intt x = 5;
+            #endregion
+            #region RunTime Error
+            //int x = 10, y = 0;
+            //Console.WriteLine(x/y);
+            #endregion
+            #region Logical Error
+            //int x = 20, y = 40;
+            //Console.WriteLine(Sum(x,y));
+            #endregion
+            #region Warning Error
+            //int x = 5;//4 Byte
+
+            #endregion
+            #endregion
         }
     }
 }
